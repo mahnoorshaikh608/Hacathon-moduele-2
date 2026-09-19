68d2377f3d3e18441e2a3d715cad56a9fbe11c52
mahnoorshaikh608 <mahnoorshaikh608@gmail.com>
2026-09-19 23:54:40 +0500

    push

--- app.js
+++ app.js
@@ -6,4 +6,25 @@ const client = createClient(supabaseUrl,supabasekey);
 
 console.log(client);
 
+let submit = document.querySelector("#submitbtn")
+console.log(submit)
 
+let  username = document.querySelector("#username")
+let  useremail = document.querySelector("#useremail")
+let  userpassword = document.querySelector("#userpassword");
+
+submit.addEventListener ("click", async(event)=>{
+    event.preventDefault()
+    try{
+const { error } = await client
+  .from('Recipe-shairing')
+  .insert([{
+    name : username.value,
+    email : useremail.value,
+
+   }])
+}
+catch(error){
+    console.log(error)
+};
+})
