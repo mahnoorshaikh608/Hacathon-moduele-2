00968c73523324284578f97bd8dfa5fbf3ea1b60
mahnoorshaikh608 <mahnoorshaikh608@gmail.com>
2026-09-20 05:32:54 +0500

    push

--- app.js
+++ app.js
@@ -7,24 +7,43 @@ const client = createClient(supabaseUrl,supabasekey);
 console.log(client);
 
 let submit = document.querySelector("#submitbtn")
-console.log(submit)
 
-let  username = document.querySelector("#username")
-let  useremail = document.querySelector("#useremail")
+
+// console.log(username);
+// console.log(useremail);
+// console.log(userpassword);
+
+if(submit) {
+
+let  username = document.querySelector("#username");
+let  useremail = document.querySelector("#useremail");
 let  userpassword = document.querySelector("#userpassword");
 
 submit.addEventListener ("click", async(event)=>{
     event.preventDefault()
+    if (!username.value || !useremail.value || !userpassword.value){
+        // alert("please fill all fields");
+        // return;
+    }
     try{
-const { error } = await client
-  .from('Recipe-shairing')
+const { data ,error } = await client
+  .from('profiles')
   .insert([{
     name : username.value,
     email : useremail.value,
 
-   }])
+   }]);
 }
 catch(error){
     console.log(error)
 };
-})
+});
+}
+
+function showOptions() {
+    document.getElementById("options").style.display = "block";
+}
+
+function logout() {
+    window.location.href = "logout.html";
+}
